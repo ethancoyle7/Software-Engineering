1965867181b99e8cd163918f1a6fd675ea30aa4a
fowzy <fwzsas@gmail.com>
2022-03-07 13:46:42 -0600

    update FA

--- js/firebaseConfig.js
+++ js/firebaseConfig.js
@@ -1,12 +1,9 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+const firebaseConfig = { 
   apiKey: "AIzaSyCRKIp_ALm8rO_hNMtZ0CXWXcXis_6198w",
   authDomain: "smartpet-28571.firebaseapp.com",
   projectId: "smartpet-28571",
@@ -17,5 +14,10 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
 const analytics = getAnalytics(app);
+
+// we need to update the firestore settings to avoid error in console
+db.settings({timestampInSnapshots: true});
