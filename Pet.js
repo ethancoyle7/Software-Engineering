be7b6691c992bfed87f21d920b4c0336c434b623
Jereen <jader9e1@hotmail.com>
2022-03-16 13:19:50 -0500

    "added new pet animation functions"

--- Pet.js
+++ Pet.js
@@ -8,6 +8,7 @@ class Pet{
     constructor(scene){
         this.petdance=null;
         this.scene =scene;
+        this.choice = '';
         //this.x=x;
         //this.y=y;
     }
@@ -33,21 +34,63 @@ class Pet{
 
 //}
 
-
-
-eggAnimation(){
-    let petdance = this.add.sprite(200, 250,"pet");
-    petdance.setScale(5);
+eggAnimation(choice){
+    let petdance = this.add.sprite(200, 250,"pet2lightattack");
+        petdance.setScale(5);
         // Create the idle animation
         petdance.anims.create({
-            key: 'idle',
-            frames: this.anims.generateFrameNumbers('pet', {
-                frames: [0, 5]
+            key: 'idle1',
+            frames: this.anims.generateFrameNumbers('pet2lightattack', {
+                start:0,
+                end:3
             }),
             frameRate: 8,
-            repeat: 2
+            repeat: -1
         });
-        petdance.anims.play('idle');
+        petdance.anims.create({
+            key: 'feed',
+            frames: this.anims.generateFrameNumbers('pet3climb', {
+                frames: [0, 3]
+            }),
+            frameRate: 8,
+            repeat: -1
+        });
+        
+        petdance.anims.play(choice);
+}
+Pet2Animation(choice){
+    let pet2dance = this.add.sprite(200,250,"pet2");
+    pet2dance.setScale(5);
+    pet2dance.anims.create({
+        key: 'idle',
+        frames: this.anims.generateFrameNumbers('pet2', {
+            frames: [0, 5]
+        }),
+        frameRate: 8,
+        repeat: 2
+    });
+    pet2dance.anims.create({
+        key: 'feed',
+        frames: this.anims.generateFrameNumbers('pet2climb', {
+            frames: [0, 5]
+        }),
+        frameRate: 8,
+        repeat: 2
+    });
+    pet2dance.anims.play(choice);
+    }
+Pet3Animation(choice){
+    let pet3dance = this.add.sprite(200,250,"pet3");
+    pet3dance.setScale(5);
+    pet3dance.anims.create({
+        key: 'idle',
+        frames: this.anims.generateFrameNumbers('pet3run', {
+            frames: [0, 5]
+        }),
+        frameRate: 8,
+        repeat: 2
+    });
+    pet3dance.anims.play(choice);
 }
 //juvinileAnimation(){
 
