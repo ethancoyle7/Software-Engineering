e4cb6bdf3f20beae9dd31a0a77a3ef78728467cc
Jereen <jader9e1@hotmail.com>
2022-03-16 23:32:06 -0500

    "modified Pet.js"

--- Pet.js
+++ Pet.js
@@ -35,26 +35,43 @@ class Pet{
 //}
 
 eggAnimation(choice){
-    let petdance = this.add.sprite(200, 250,"pet2lightattack");
+    let petdance = this.add.sprite(200, 250,"pet");
         petdance.setScale(5);
         // Create the idle animation
         petdance.anims.create({
             key: 'idle1',
-            frames: this.anims.generateFrameNumbers('pet2lightattack', {
+            frames: this.anims.generateFrameNumbers('pet', {
                 start:0,
-                end:3
+                end:5
             }),
-            frameRate: 8,
-            repeat: -1
+            frameRate: 12,
+            repeat: 3
         });
         petdance.anims.create({
             key: 'feed',
-            frames: this.anims.generateFrameNumbers('pet3climb', {
+            frames: this.anims.generateFrameNumbers('petclimb', {
                 frames: [0, 3]
             }),
             frameRate: 8,
             repeat: -1
         });
+        petdance.anims.create({
+            key: 'bathe+',
+            frames: this.anims.generateFrameNumbers('petthrow', {
+                frames: [0, 1]
+            }),
+            frameRate: 3,
+            repeat: -1
+        });
+        petdance.anims.create({
+            key: 'health+',
+            frames: this.anims.generateFrameNumbers('winningpet', {
+                start:0,
+                end: 7
+            }),
+            frameRate: 8,
+            repeat: -1
+        });        
         
         petdance.anims.play(choice);
 }
@@ -62,21 +79,38 @@ Pet2Animation(choice){
     let pet2dance = this.add.sprite(200,250,"pet2");
     pet2dance.setScale(5);
     pet2dance.anims.create({
-        key: 'idle',
+        key: 'idle1',
         frames: this.anims.generateFrameNumbers('pet2', {
             frames: [0, 5]
         }),
         frameRate: 8,
         repeat: 2
     });
-    pet2dance.anims.create({
+    petdance.anims.create({
         key: 'feed',
         frames: this.anims.generateFrameNumbers('pet2climb', {
-            frames: [0, 5]
+            frames: [0, 3]
         }),
         frameRate: 8,
-        repeat: 2
+        repeat: -1
     });
+    petdance.anims.create({
+        key: 'bathe+',
+        frames: this.anims.generateFrameNumbers('pet2throw', {
+            frames: [0, 1]
+        }),
+        frameRate: 3,
+        repeat: -1
+    });
+    petdance.anims.create({
+        key: 'health+',
+        frames: this.anims.generateFrameNumbers('pet2winningpet', {
+            start:0,
+            end: 7
+        }),
+        frameRate: 8,
+        repeat: -1
+    });  
     pet2dance.anims.play(choice);
     }
 Pet3Animation(choice){
@@ -84,12 +118,37 @@ Pet3Animation(choice){
     pet3dance.setScale(5);
     pet3dance.anims.create({
         key: 'idle',
-        frames: this.anims.generateFrameNumbers('pet3run', {
+        frames: this.anims.generateFrameNumbers('pet3', {
             frames: [0, 5]
         }),
         frameRate: 8,
         repeat: 2
     });
+    petdance.anims.create({
+        key: 'feed',
+        frames: this.anims.generateFrameNumbers('pet3climb', {
+            frames: [0, 3]
+        }),
+        frameRate: 8,
+        repeat: -1
+    });
+    petdance.anims.create({
+        key: 'bathe+',
+        frames: this.anims.generateFrameNumbers('pet3throw', {
+            frames: [0, 1]
+        }),
+        frameRate: 3,
+        repeat: -1
+    });
+    petdance.anims.create({
+        key: 'health+',
+        frames: this.anims.generateFrameNumbers('pet3winningpet', {
+            start:0,
+            end: 7
+        }),
+        frameRate: 8,
+        repeat: -1
+    });  
     pet3dance.anims.play(choice);
 }
 //juvinileAnimation(){
