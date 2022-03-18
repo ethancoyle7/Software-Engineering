cbc91abadff1f15a44c801e7065f0da4303faed1
ethancoyle7 <77364000+ethancoyle7@users.noreply.github.com>
2022-03-18 13:08:27 -0500

    mispelled grilledcheese

--- MainScene.js
+++ MainScene.js
@@ -28,6 +28,7 @@ class MainScene extends Phaser.Scene {
         // this.load.audio('bgmusic', './assets/gamemusic.mp3');
 
         //load the images for the food
+        // const food = ["bun", "soup", "sandwich1", "sandwich2", "pancake2", "OJ", "orange","grilledcheese","eggs","drink2","drink","donut","cupcake","chicken", "pancake"];
         this.load.image('pancake', './assets/food/actualpancake.png')
         this.load.image('chicken', './assets/food/chicken.png')
         this.load.image('cupcake', './assets/food/cupcake.png')
@@ -35,7 +36,7 @@ class MainScene extends Phaser.Scene {
         this.load.image('drink', './assets/food/drink.png')
         this.load.image('drink2', './assets/food/drink2.png')
         this.load.image('eggs', './assets/food/egg.png')
-        this.load.image('grilledcheese', './assets/food/grilledhceese.png')
+        this.load.image('grilledcheese', './assets/food/grilledhcheese.png')
         this.load.image('orange', './assets/food/orange.png')
         this.load.image('OJ', './assets/food/orangejuice.png')
         this.load.image('pancake2', './assets/food/pancake.png')
