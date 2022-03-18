9ab2edd97443f3a8d11c8a37ad9b441568bbceb4
Jereen <jader9e1@hotmail.com>
2022-03-17 20:15:24 -0500

    "more image options"

--- bootpet.js
+++ bootpet.js
@@ -1,25 +1,102 @@
 const BOOTPET = [
+    // {
+    //     name: "Frame1",
+    //     image: "catA.png",
+
+    // },
+    // {
+    //     name: "Frame2",
+    //     image: "catB.png",
+
+    // },
+    // {
+    //     name: "Frame3",
+    //     image: "catC.png",
+
+    // },
+    // {
+    //     name: "Frame4",
+    //     image: "catD.png",
+
+    // },
     {
-        name: "Frame1",
-        image: "catA.png",
+
+        name: "Frame5",
+        image: "pet1A.png",
 
     },
     {
-        name: "Frame2",
-        image: "catB.png",
+
+        name: "Frame6",
+        image: "pet1B.png",
 
     },
     {
-        name: "Frame3",
-        image: "catC.png",
+
+        name: "Frame7",
+        image: "pet1C.png",
 
     },
     {
-        name: "Frame4",
-        image: "catD.png",
+
+        name: "Frame8",
+        image: "pet1D.png",
 
     },
+    // {
 
+    //     name: "Frame9",
+    //     image: "jump1.png",
 
+    // },
+    // {
+
+    //     name: "Frame10",
+    //     image: "jump2.png",
+
+    // },
+    // {
+
+    //     name: "Frame11",
+    //     image: "jump3.png",
+
+    // },
+    // {
+
+    //     name: "Frame12",
+    //     image: "jump4.png",
+
+    // },
+    // {
+
+    //     name: "Frame13",
+    //     image: "jump5.png",
+
+    // },
+    // {
+
+    //     name: "Frame14",
+    //     image: "jump6.png",
+
+    // },
+    // {
+
+    //     name: "Frame15",
+    //     image: "jump7.png",
+
+    // },
+    // {
+
+    //     name: "Frame16",
+    //     image: "jump8.png",
+
+    // },
+    // {
+
+    //     name: "Frame17",
+    //     image: "jump9.png",
+
+    // },
+   
 
 ];
