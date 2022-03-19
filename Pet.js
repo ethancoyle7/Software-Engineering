8156ee823ef7a5d7920b7eb4639f66f9d4d3015a
Jereen <jader9e1@hotmail.com>
2022-03-19 05:05:31 -0500

    "pet class fixes"

--- Pet.js
+++ Pet.js
@@ -4,129 +4,147 @@ class Pet{
      * @param {Phaser.Scene} scene 
      */
     constructor(scene){
-        this.petdance=null;
+        this.pet1=null;
         this.scene =scene;
         this.choice = '';
         
     }
-// some pet animations for actions
-eggAnimation(choice){
-    let petdance = this.add.sprite(200, 250,"pet");
-        petdance.setScale(5);
-        // Create the idle animation
-        petdance.anims.create({
-            key: 'idle1',
-            frames: this.anims.generateFrameNumbers('pet', {
-                start:0,
-                end:5
-            }),
-            frameRate: 12,
-            repeat: 3
+    // some pet animations for actions
+    Pet1Animation(choice){
+        let pet1 = this.add.sprite(200, 250,"pet");
+            pet1.setScale(5);
+            //make the pet interactive and movable
+            pet1.setInteractive({ draggable: true });
+            // to know the item is selected change the color of the item
+            this.input.on('dragstart', function (pointer, gameObject) {
+                gameObject.setTint(0xff0000);
+            });
+            // Create the idle animation
+            pet1.anims.create({
+                key: 'idle1',
+                frames: this.anims.generateFrameNumbers('pet', {
+                    start:0,
+                    end:5
+                }),
+                frameRate: 12,
+                repeat: -1
+            });
+            pet1.anims.create({
+                key: 'feed',
+                frames: this.anims.generateFrameNumbers('petclimb', {
+                    frames: [0, 3]
+                }),
+                frameRate: 8,
+                repeat: -1
+            });
+            pet1.anims.create({
+                key: 'bathe+',
+                frames: this.anims.generateFrameNumbers('petthrow', {
+                    frames: [0, 1]
+                }),
+                frameRate: 3,
+                repeat: -1
+            });
+            pet1.anims.create({
+                key: 'health+',
+                frames: this.anims.generateFrameNumbers('winningpet', {
+                    start:0,
+                    end: 7
+                }),
+                frameRate: 8,
+                repeat: -1
+            });        
+            
+            pet1.anims.play(choice);
+    }
+    Pet2Animation(choice){
+        let pet2 = this.add.sprite(200,250,"pet2run");
+        pet2.setScale(5);
+        //make the pet interactive and movable
+        pet2.setInteractive({ draggable: true });
+        // to know the item is selected change the color of the item
+        this.input.on('dragstart', function (pointer, gameObject) {
+            gameObject.setTint(0xff0000);
         });
-        petdance.anims.create({
+        pet2.anims.create({
+            key: 'idle1',
+            frames: this.anims.generateFrameNumbers('pet2run', {
+                frames: [0, 5]
+            }),
+            frameRate: 8,
+            repeat: -1
+        });
+        pet2.anims.create({
             key: 'feed',
-            frames: this.anims.generateFrameNumbers('petclimb', {
+            frames: this.anims.generateFrameNumbers('pet2climb', {
                 frames: [0, 3]
             }),
             frameRate: 8,
             repeat: -1
         });
-        petdance.anims.create({
+        pet2.anims.create({
             key: 'bathe+',
-            frames: this.anims.generateFrameNumbers('petthrow', {
+            frames: this.anims.generateFrameNumbers('pet2throw', {
                 frames: [0, 1]
             }),
             frameRate: 3,
             repeat: -1
         });
-        petdance.anims.create({
+        pet2.anims.create({
             key: 'health+',
-            frames: this.anims.generateFrameNumbers('winningpet', {
+            frames: this.anims.generateFrameNumbers('pet2winningpet', {
                 start:0,
                 end: 7
             }),
             frameRate: 8,
             repeat: -1
-        });        
-        
-        petdance.anims.play(choice);
-}
-Pet2Animation(choice){
-    let pet2dance = this.add.sprite(200,250,"pet2");
-    pet2dance.setScale(5);
-    pet2dance.anims.create({
-        key: 'idle1',
-        frames: this.anims.generateFrameNumbers('pet2', {
-            frames: [0, 5]
-        }),
-        frameRate: 8,
-        repeat: 2
-    });
-    petdance.anims.create({
-        key: 'feed',
-        frames: this.anims.generateFrameNumbers('pet2climb', {
-            frames: [0, 3]
-        }),
-        frameRate: 8,
-        repeat: -1
-    });
-    pet2dance.anims.create({
-        key: 'bathe+',
-        frames: this.anims.generateFrameNumbers('pet2throw', {
-            frames: [0, 1]
-        }),
-        frameRate: 3,
-        repeat: -1
-    });
-    pet2dance.anims.create({
-        key: 'health+',
-        frames: this.anims.generateFrameNumbers('pet2winningpet', {
-            start:0,
-            end: 7
-        }),
-        frameRate: 8,
-        repeat: -1
-    });  
-    pet2dance.anims.play(choice);
+        });  
+        pet2.anims.play(choice);
+        }
+    Pet3Animation(choice){
+        let pet3 = this.add.sprite(200,250,"pet3");
+        pet3.setScale(5);
+        //make the pet interactive and movable
+        pet3.setInteractive({ draggable: true });
+        // to know the item is selected change the color of the item
+        this.input.on('dragstart', function (pointer, gameObject) {
+            gameObject.setTint(0xff0000);
+        });
+        pet3.anims.create({
+            key: 'idle1',
+            frames: this.anims.generateFrameNumbers('pet3', {
+                frames: [0, 5]
+            }),
+            frameRate: 7,
+            repeat: -1
+        });
+        pet3.anims.create({
+            key: 'feed',
+            frames: this.anims.generateFrameNumbers('pet3climb', {
+                frames: [0, 3]
+            }),
+            frameRate: 8,
+            repeat: -1
+        });
+        pet3.anims.create({
+            key: 'bathe+',
+            frames: this.anims.generateFrameNumbers('pet3throw', {
+                frames: [0, 1]
+            }),
+            frameRate: 3,
+            repeat: -1
+        });
+        pet3.anims.create({
+            key: 'health+',
+            frames: this.anims.generateFrameNumbers('pet3winningpet', {
+                start:0,
+                end: 7
+            }),
+            frameRate: 8,
+            repeat: -1
+        });  
+        pet3.anims.play(choice);
     }
-Pet3Animation(choice){
-    let pet3dance = this.add.sprite(200,250,"pet3");
-    pet3dance.setScale(5);
-    pet3dance.anims.create({
-        key: 'idle',
-        frames: this.anims.generateFrameNumbers('pet3', {
-            frames: [0, 5]
-        }),
-        frameRate: 8,
-        repeat: 2
-    });
-    pet3dance.anims.create({
-        key: 'feed',
-        frames: this.anims.generateFrameNumbers('pet3climb', {
-            frames: [0, 3]
-        }),
-        frameRate: 8,
-        repeat: -1
-    });
-    pet3dance.anims.create({
-        key: 'bathe+',
-        frames: this.anims.generateFrameNumbers('pet3throw', {
-            frames: [0, 1]
-        }),
-        frameRate: 3,
-        repeat: -1
-    });
-    pet3dance.anims.create({
-        key: 'health+',
-        frames: this.anims.generateFrameNumbers('pet3winningpet', {
-            start:0,
-            end: 7
-        }),
-        frameRate: 8,
-        repeat: -1
-    });  
-    pet3dance.anims.play(choice);
-}
 
 
-}
+    }
